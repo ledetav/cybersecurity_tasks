d91f596eb11bf76b3b1ca27783d903a8da31bb85
Alexander Break <92530287+ledetav@users.noreply.github.com>
2025-04-05 23:40:48 +0300

    Update improved_tabular_cipher_tests.py

--- improved_tabular_cipher/improved_tabular_cipher_tests.py
+++ improved_tabular_cipher/improved_tabular_cipher_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from cipher import encrypt, decrypt
+from improved_tabular_cipher import encrypt_tabular, decrypt_tabular
 
 class TestEnhancedTableCipher(unittest.TestCase):
 
@@ -7,40 +7,40 @@ class TestEnhancedTableCipher(unittest.TestCase):
         message = "HELLOENCRYPTION"
         key = "KEY"
         expected_length = len(message)
-        result = encrypt(message, key)
+        result = encrypt_tabular(message, key)
         self.assertEqual(len(result), expected_length)
 
     def test_decrypt_returns_original_message(self):
         message = "HELLOENCRYPTION"
         key = "KEY"
-        encrypted = encrypt(message, key)
-        decrypted = decrypt(encrypted, key)
+        encrypted = encrypt_tabular(message, key)
+        decrypted = decrypt_tabular(encrypted, key)
         self.assertEqual(decrypted, message)
 
     def test_encrypt_with_padding(self):
         message = "TEST"
         key = "LONGKEY"
-        encrypted = encrypt(message, key)
+        encrypted = encrypt_tabular(message, key)
         self.assertTrue('_' in encrypted)
 
     def test_decrypt_removes_padding(self):
         message = "TEST"
         key = "LONGKEY"
-        encrypted = encrypt(message, key)
-        decrypted = decrypt(encrypted, key)
+        encrypted = encrypt_tabular(message, key)
+        decrypted = decrypt_tabular(encrypted, key)
         self.assertEqual(decrypted, message)
 
     def test_encrypt_empty_string(self):
-        self.assertEqual(encrypt("", "KEY"), "")
+        self.assertEqual(encrypt_tabular("", "KEY"), "")
 
     def test_decrypt_empty_string(self):
-        self.assertEqual(decrypt("", "KEY"), "")
+        self.assertEqual(decrypt_tabular("", "KEY"), "")
 
     def test_encrypt_single_char(self):
-        self.assertEqual(encrypt("A", "B"), "A")
+        self.assertEqual(encrypt_tabular("A", "B"), "A")
 
     def test_decrypt_single_char(self):
-        self.assertEqual(decrypt("A", "B"), "A")
+        self.assertEqual(decrypt_tabular("A", "B"), "A")
 
 
 if __name__ == '__main__':
